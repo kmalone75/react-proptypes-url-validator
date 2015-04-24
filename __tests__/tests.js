b8a3895cc667f495ae8ad411dfb5b9ba46b1b440
Brendan Irvine-Broque <brendanib@gmail.com>
2015-04-24 10:35:36 -0700

    tests pass

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -6,15 +6,30 @@
 
 jest.dontMock('../index.js');
 describe('react-proptypes-url-validator', function() {
+
+  // Boilerplate to simplify assertions for warnings
+  var warn, warnings;
+  beforeEach(function () {
+    warn = console.warn;
+    warnings = [];
+    console.warn = function () {
+      warnings.push(Array.prototype.join.call(arguments, ' '));
+    };
+  });
+  afterEach(function () {
+    console.warn = warn;
+  });
+
   it('returns Error if validation fails', function() {
     var React = require('react/addons');
     var TestUtils = React.addons.TestUtils;
     var validateURL = require('../index.js');
     var invalidURL = 'httpz://foo/doo.boodoo';
+    var expectedWarning = 'Invalid url supplied to Component'
 
     var Component = React.createClass({
       propTypes: {
-        url: validateURL(props, propName, componentName)
+        url: validateURL
       },
 
       render: function() {
@@ -24,10 +39,8 @@ describe('react-proptypes-url-validator', function() {
 
     TestUtils.renderIntoDocument(<Component url={invalidURL} />);
 
-    expect(console.error.argsForCall.length).toBe(1);
-    expect(console.error.argsForCall[0][0]).toBe(
-      'Warning: Invalid url supplied to `Component`'
-    );
+    expect(warnings.length).toEqual(1);
+    expect(warnings[0]).toContain(expectedWarning);
 
   });
 });
